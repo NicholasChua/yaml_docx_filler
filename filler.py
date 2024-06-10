d82e2cc167607c79781c141ef7d98010cf3ae5e2
NicholasChua <7358234+NicholasChua@users.noreply.github.com>
2024-06-10 16:21:42 +0800

    feat: change returned data type for single items

--- filler.py
+++ filler.py
@@ -143,18 +143,23 @@ def fill_common_items(yaml_content: DocumentType) -> dict:
     return context
 
 
-def get_mixed_types_str_keys(content: List[Any]) -> List[str]:
+def get_mixed_types_str_keys(
+    content: Union[List[Any], Dict[Any, Any]]
+) -> Union[List[str], str]:
     """
-    Helper function to extract a list of strings and dictionary keys from a list of mixed strings and dictionaries.
-    Usage: get_mixed_types_str_keys(content['list']['sub-list'])
+    Helper function to extract a list of strings and dictionary keys from a list or dictionary of mixed strings and/or dictionaries.
+    Usage: get_mixed_types_str_keys(content['list']['sub-list']) or get_mixed_types_str_keys(content['dictionary'])
 
     Inputs:
-    content (list): The list containing mixed strings and dictionaries
+    content (list, dict): The list or dictionary containing mixed strings and/or dictionaries
 
     Returns:
-    mixed_str_key_list (list): A list of strings and dictionary keys
+    mixed_str_key_list (list, str): A list of strings and dictionary keys. If there is only one item, it is returned as a string instead of a list.
     """
     mixed_str_key_list = []
+    # Interpret the content as a list if it is a dictionary
+    if isinstance(content, dict):
+        content = list(content.values())
     for item in content:
         # If the item is a string, strip the trailing newline and add it to the list
         if isinstance(item, str):
@@ -166,6 +171,11 @@ def get_mixed_types_str_keys(content: List[Any]) -> List[str]:
         # Default case handling
         else:
             mixed_str_key_list.append("error here")
+
+    # If there is only one item in the list, return it as a string.
+    if len(mixed_str_key_list) == 1:
+        return str(mixed_str_key_list[0])
+
     return mixed_str_key_list
 
 
@@ -180,7 +190,8 @@ def get_mixed_types_dict_values(content: List[Any]) -> List[str]:
     content (list): The list containing mixed strings and dictionaries
 
     Returns:
-    dict_values: A list of lists containing the values of the dictionaries in the input list
+    dict_values (list): A list of lists containing the values of the dictionaries in the input list. If there is only one list within the list,
+    it is returned as a single list instead of a list of lists.
     """
     dict_count = 0
     dict_values = []
@@ -192,45 +203,50 @@ def get_mixed_types_dict_values(content: List[Any]) -> List[str]:
                 temp_list.append(value)
             dict_values.append(temp_list)
 
+    # If no dictionaries are found in the list, return None
     if dict_count == 0:
         print("No dictionaries found in the list")
         return None
 
+    # If there is only one list of dictionary values, return it as a single list
+    if len(dict_values) == 1:
+        return dict_values[0]
+
     return dict_values
 
 
-def get_multi_level_content(
-    content: List[Any],
-) -> List[Union[List[str], List[List[Dict[str, Union[str, Dict[str, str]]]]]]]:
-    """
-    Helper function that returns a list of lists, the first contains top-level strings and dictionary keys.
-    The second contains a list of lists of dictionary values.
-    Used to form a data structure allowing access to multi-level content in a predictable way.
-    Usage: get_multi_level_content(content['list']['sub-list'])
+# def get_multi_level_content(
+#     content: List[Any],
+# ) -> List[Union[List[str], List[List[Dict[str, Union[str, Dict[str, str]]]]]]]:
+#     """
+#     Helper function that returns a list of lists, the first contains top-level strings and dictionary keys.
+#     The second contains a list of lists of dictionary values.
+#     Used to form a data structure allowing access to multi-level content in a predictable way.
+#     Usage: get_multi_level_content(content['list']['sub-list'])
 
-    E.g. To get content from the top-level > sub-list > sub-sub-list2,
-    use the following structure of indices:
-    - top-level: content['list']
-        - sub-list-item-1: content['list']['sub-list'][0]
-            - sub-sub-list1: content['list']['sub-list'][0][0]
-                - sub-sub-sub-list1: content['list']['sub-list'][0][0][0]
-        - sub-list-item-2: content['list']['sub-list'][1]
+#     E.g. To get content from the top-level > sub-list > sub-sub-list2,
+#     use the following structure of indices:
+#     - top-level: content['list']
+#         - sub-list-item-1: content['list']['sub-list'][0]
+#             - sub-sub-list1: content['list']['sub-list'][0][0]
+#                 - sub-sub-sub-list1: content['list']['sub-list'][0][0][0]
+#         - sub-list-item-2: content['list']['sub-list'][1]
 
-    Inputs:
-    content (list): The list containing mixed strings and dictionaries
+#     Inputs:
+#     content (list): The list containing mixed strings and dictionaries
 
-    Returns:
-    list_of_lists (list): A list of lists containing the top-level strings and dictionary keys and the dictionary values
-    """
-    list_of_lists = []
-    # Get all the strings and dictionary keys and append them to the list
-    top_level_str = get_mixed_types_str_keys(content)
-    list_of_lists.append(top_level_str)
-    # Get all the dictionary values and append them to the list
-    dict_values = get_mixed_types_dict_values(content)
-    list_of_lists.append(dict_values)
+#     Returns:
+#     list_of_lists (list): A list of lists containing the top-level strings and dictionary keys and the dictionary values
+#     """
+#     list_of_lists = []
+#     # Get all the strings and dictionary keys and append them to the list
+#     top_level_str = get_mixed_types_str_keys(content)
+#     list_of_lists.append(top_level_str)
+#     # Get all the dictionary values and append them to the list
+#     dict_values = get_mixed_types_dict_values(content)
+#     list_of_lists.append(dict_values)
 
-    return list_of_lists
+#     return list_of_lists
 
 
 def example_document_generation(
@@ -250,8 +266,42 @@ def example_document_generation(
 
     Returns:
     None
-    """
 
+    For the example document, the data structure is as follows:
+    {'type': 'EXAMPLE DOCUMENT', 
+    'document_no': 'EX01-100', 
+    'document_code': 'EX01-100-01', 
+    'effective_date': '06-JUN-2024', 
+    'document_rev': '00', 
+    'title': 'Example Document', 
+    'revision_history': [{'revision_no': '00', 'description_of_changes': 'Initial release', 'effective_date': '06-JUN-2024'}], 
+    'prepared_by': [{'name': 'Nicholas Chua', 'title': 'Preparer'}], 
+    'reviewed_approved_by': [{'name': 'Not Nicholas Chua', 'title': 'Approver'}], 
+    'purpose': ['Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'], 
+    'scope': ['Lorem ipsum dolor sit.'], 
+    'responsibility': ['Lorem ipsum,', 'dolor sit amet.'], 
+    'definition': ['Lorem: ipsum,', 'Dolor: sit amet.'], 
+    'procedure': {
+    'example_policy': 
+    [
+        {
+        'Lorem ipsum dolor sit amet:': ['consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
+        }, 
+        {
+        '"This is a very very very very long multi-line comment line 1.\nThis is a very very very very long multi-line comment line 2:"\n': ['This is a sub-item of the multi-line comment.']
+        }, 
+        'Lorem ipsum dolor sit amet.'
+    ], 
+    'another_policy': 
+    [
+        {
+        'I have 9 sub items:': ['Item 1.', 'Item 2.', 'Item 3.', 'Item 4.', 'Item 5.', 'Item 6.', 'Item 7.', 'Item 8.', 'Item 9.']
+        }
+    ]
+    }, 
+    'reference': ['N/A'], 
+    'attachment': ['N/A']}
+    """
     # Open the Word document
     doc = DocxTemplate(input_file)
 
@@ -263,27 +313,33 @@ def example_document_generation(
     context.update(
         {
             # Splitting up the Procedure section into multiple levels due to its complexity
-            "example_policy_1": str(
-                get_mixed_types_str_keys(yaml_content["procedure"]["example_policy"])[0]
-            ).strip("['']"),
+            # Returns: Lorem ipsum dolor sit amet:
+            "example_policy_1": get_mixed_types_str_keys(
+                yaml_content["procedure"]["example_policy"]
+            )[0],
+            # Returns: ['consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
             "sub_example_policy_1": get_mixed_types_dict_values(
                 yaml_content["procedure"]["example_policy"]
             )[0],
-            "example_policy_2": str(
+            # Returns: "This is a very very very very long multi-line comment line 1. \
+            # \ This is a very very very very long multi-line comment line 2:"
+            "example_policy_2": (
                 get_mixed_types_str_keys(yaml_content["procedure"]["example_policy"])[1]
-            ).strip("['']"),
-            "sub_example_policy_2": get_mixed_types_dict_values(
-                yaml_content["procedure"]["example_policy"]
-            )[1],
+            ).rstrip("\n"),
+            # Returns: This is a sub-item of the multi-line comment.
+            "sub_example_policy_2": get_mixed_types_dict_values(yaml_content["procedure"]["example_policy"])[1],
+            # Returns: Lorem ipsum dolor sit.
             "example_policy_3": str(
                 get_mixed_types_str_keys(yaml_content["procedure"]["example_policy"])[2]
             ).strip("['']"),
-            "another_policy_1": str(
-                get_mixed_types_str_keys(yaml_content["procedure"]["another_policy"])
-            ).strip("['']"),
+            # Returns: I have 9 sub items:
+            "another_policy_1": get_mixed_types_str_keys(
+                yaml_content["procedure"]["another_policy"]
+            ),
+            # Returns: ['Item 1.', 'Item 2.', 'Item 3.', 'Item 4.', 'Item 5.', 'Item 6.', 'Item 7.', 'Item 8.', 'Item 9.']
             "sub_another_policy_1": get_mixed_types_dict_values(
                 yaml_content["procedure"]["another_policy"]
-            )[0],
+            ),
         }
     )
 
